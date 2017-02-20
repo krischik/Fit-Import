854db401eb004d7261f340f9cd2c0f12886e9183
Martin Krischik <krischik@users.sourceforge.net>
2017-02-20 08:10:12 +0100

    the .bat files are generated and need not to be checkend in.

--- src/main/scripts/Device-Activate-Logging.bat
+++
@@ -1,5 +0,0 @@
-PUSHD %PROJECT_HOME%
-  adb -s 0149E08204021014 shell setprop log.tag.Main VERBOSE
-  adb -s 0149E08204021014 shell setprop log.tag.KrischikLog VERBOSE
-  adb -s 0149E08204021014 shell setprop log.tag.ik.fit_import.PackageKt VERBOSE
-POPD
